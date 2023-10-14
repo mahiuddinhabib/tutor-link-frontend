f9822447ccfebc31684cf76d33b1b0c2cf71312f
mahiuddinhabib <mh.habib137@gmail.com>
2023-10-14 18:15:18 +0600

    basic structured done

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { redirect } from "next/navigation";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>This is the HOME page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
